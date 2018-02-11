515d1179da72418d2da1eaa11dadcb64d8bef5c0
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-02-10 22:09:53 -0800

    updated code

--- letter.js
+++ letter.js
@@ -6,19 +6,20 @@ function Letter(letter){
     this.guessedLetter = false;
 
     this.showletter = function(){
-        //puts a blank space is there is a space the the word being guessed
+        //puts a blank space if there is a space in the word that's being guessed
         if (this.currentLetter === " "){
             this.guessedLetter = true
             return " ";
         }
-        //will show underline and letter
-        if (this.guessedLetter === true){
+        //will show underline and letter if true
+        if (this.guessedLetter === false){
             return "_";
         } else {
             return this.currentLetter;
         }
+        console.log("this.currentLetter");
     }
 };
-
+ var testOne = new Letter("d");
 //export file to be used in Word.js
 module.exports = Letter;
